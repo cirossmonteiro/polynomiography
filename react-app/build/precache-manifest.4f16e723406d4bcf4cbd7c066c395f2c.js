3ffeadf211110ea47f7e9a74c6c16e7b60ca2566
Ciro Monteiro <cirossmonteiro@gmail.com>
2019-06-20 19:48:37 -0300

    build

---
+++ react-app/build/precache-manifest.4f16e723406d4bcf4cbd7c066c395f2c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "72e042d22d2173ce8d22",
+    "url": "./static/js/main.5548208f.chunk.js"
+  },
+  {
+    "revision": "95cfdd270876b5df7a71",
+    "url": "./static/js/2.99b5c1ff.chunk.js"
+  },
+  {
+    "revision": "72e042d22d2173ce8d22",
+    "url": "./static/css/main.1127bbb9.chunk.css"
+  },
+  {
+    "revision": "ab7a60d39516077cb5036e7cf9493e16",
+    "url": "./index.html"
+  }
+];
